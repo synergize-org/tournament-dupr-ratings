f6e85ed6e2b4ab54c1e7701ed33305234181f0c8
synergize <err0r142@gmail.com>
2026-06-24 17:32:59 -0600

    feat: add ConsoleOutput and CsvOutput

---
+++ Output/ConsoleOutput.cs
@@ -0,0 +1,25 @@
+using TournamentDuprRatings.Models;
+
+namespace TournamentDuprRatings.Output;
+
+public static class ConsoleOutput
+{
+    public static void PrintTable(List<TeamResult> teams)
+    {
+        Console.WriteLine();
+        Console.WriteLine(
+            $"{"#",-4} {"Player 1",-26} {"P1 DUPR ID",-12} {"P1 Dbl",-8} {"P1 Sgl",-8} " +
+            $"{"Player 2",-26} {"P2 DUPR ID",-12} {"P2 Dbl",-8} {"P2 Sgl"}");
+        Console.WriteLine(new string('-', 122));
+
+        for (int i = 0; i < teams.Count; i++)
+        {
+            var t = teams[i];
+            Console.WriteLine(
+                $"{i + 1,-4} {t.Player1Name,-26} {t.Player1DuprId ?? "",-12} {t.Player1Doubles,-8} {t.Player1Singles,-8} " +
+                $"{t.Player2Name,-26} {t.Player2DuprId ?? "",-12} {t.Player2Doubles,-8} {t.Player2Singles}");
+        }
+
+        Console.WriteLine();
+    }
+}
